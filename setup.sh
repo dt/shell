15a414d040ff693ec147b86d5131ffc2ffd0500a
David Taylor <tinystatemachine@gmail.com>
2015-02-10 22:17:05 +0000

    blow away existing inputrc

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@
 ln -sf ~/.shell/profile.sh ~/.profile
 ln -sf ~/.shell/profile.sh ~/.bash_profile
 ln -sf ~/.shell/bashrc.sh ~/.bashrc
-ln -s ~/.shell/inputrc ~/.inputrc
+ln -sf ~/.shell/inputrc ~/.inputrc
