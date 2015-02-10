38c0bb845520a0e333b71ce5a24ad2fb3407a0e5
David Taylor <tinystatemachine@gmail.com>
2015-02-10 23:02:35 +0000

    add ssh config

--- setup.sh
+++ setup.sh
@@ -4,3 +4,5 @@ ln -sf ~/.shell/profile.sh ~/.profile
 ln -sf ~/.shell/profile.sh ~/.bash_profile
 ln -sf ~/.shell/bashrc.sh ~/.bashrc
 ln -sf ~/.shell/inputrc ~/.inputrc
+mkdir -p ~/.ssh
+ln -sf ~/.shell/ssh_config ~/.ssh/config
