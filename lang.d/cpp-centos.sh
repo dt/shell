78d6a79775931f686f0499731c1405e59e49cc3b
David Taylor <tinystatemachine@gmail.com>
2015-09-24 01:37:29 +0000

    override cpp when centos has optional devtools installed
    the old one is too old, and this is where the blessed newer ones go

---
+++ lang.d/cpp-centos.sh
@@ -0,0 +1,5 @@
+if [[ -d /opt/centos/devtoolset-1.1/ ]]; then
+  export CC=/opt/centos/devtoolset-1.1/root/usr/bin/gcc
+  export CPP=/opt/centos/devtoolset-1.1/root/usr/bin/cpp
+  export CXX=/opt/centos/devtoolset-1.1/root/usr/bin/c++
+fi
