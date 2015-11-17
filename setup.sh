18dafd353f73ad3cc20eedecd44805c567cf2598
David Taylor <tinystatemachine@gmail.com>
2015-11-17 02:14:00 +0000

    include git config in dotfiles

--- setup.sh
+++ setup.sh
@@ -4,5 +4,6 @@ ln -sf ~/.shell/profile.sh ~/.profile
 ln -sf ~/.shell/profile.sh ~/.bash_profile
 ln -sf ~/.shell/bashrc.sh ~/.bashrc
 ln -sf ~/.shell/inputrc ~/.inputrc
+ln -sf ~/.shell/gitconfig ~/.gitconfig
 mkdir -p ~/.ssh
 ln -sf ~/.shell/ssh_config ~/.ssh/config
