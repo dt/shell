d0ba8384f8fc7a070dac5ef3a98de63e04a321d4
David Taylor <tinystatemachine@gmail.com>
2016-01-11 23:04:38 +0000

    wrap google cloud stuff in conditional

--- lang.d/google.sh
+++ lang.d/google.sh
@@ -1,6 +1,9 @@
 
-# The next line updates PATH for the Google Cloud SDK.
-source '/Users/david/code/google-cloud-sdk/path.bash.inc'
+if [[ -d /Users/david/code/google-cloud-sdk ]]; then
 
-# The next line enables shell command completion for gcloud.
-source '/Users/david/code/google-cloud-sdk/completion.bash.inc'
+	# The next line updates PATH for the Google Cloud SDK.
+	source '/Users/david/code/google-cloud-sdk/path.bash.inc'
+
+	# The next line enables shell command completion for gcloud.
+	source '/Users/david/code/google-cloud-sdk/completion.bash.inc'
+fi
