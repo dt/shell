f7223072bc97cc81740a4187924f5272c8e8cef4
David Taylor <tinystatemachine@gmail.com>
2016-05-10 19:14:27 +0000

    global git ignore

--- setup.sh
+++ setup.sh
@@ -5,5 +5,6 @@ ln -sf ~/.shell/profile.sh ~/.bash_profile
 ln -sf ~/.shell/bashrc.sh ~/.bashrc
 ln -sf ~/.shell/inputrc ~/.inputrc
 ln -sf ~/.shell/gitconfig ~/.gitconfig
+ln -sf ~/.shell/gitignore_global ~/.gitignore_global
 mkdir -p ~/.ssh
 ln -sf ~/.shell/ssh_config ~/.ssh/config
