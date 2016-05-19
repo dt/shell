bf19148155cb0d43d45a485ae768ac7636ff2b89
David Taylor <tinystatemachine@gmail.com>
2016-05-19 21:47:31 +0000

    delete centos stuff

--- lang.d/cpp-centos.sh
+++
@@ -1,5 +0,0 @@
-if [[ -d /opt/centos/devtoolset-1.1/ ]]; then
-  export CC=/opt/centos/devtoolset-1.1/root/usr/bin/gcc
-  export CPP=/opt/centos/devtoolset-1.1/root/usr/bin/cpp
-  export CXX=/opt/centos/devtoolset-1.1/root/usr/bin/c++
-fi
