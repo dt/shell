fd16346ecf165083b247833f4044efaf0b48bad1
David Taylor <david@null.local>
2016-06-01 17:38:07 +0000

    move confs into own dir

--- setup.sh
+++ setup.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+echo "Setting up bash scripts"
 ln -sf ~/.shell/profile.sh ~/.profile
 ln -sf ~/.shell/profile.sh ~/.bash_profile
 ln -sf ~/.shell/bashrc.sh ~/.bashrc
 ln -sf ~/.shell/inputrc ~/.inputrc
-ln -sf ~/.shell/gitconfig ~/.gitconfig
-ln -sf ~/.shell/gitignore_global ~/.gitignore_global
+
+echo "Setting up git configs scripts"
+ln -sf ~/.shell/confs/gitconfig ~/.gitconfig
+ln -sf ~/.shell/confs/gitignore_global ~/.gitignore_global
+
+echo "Setting up ssh config"
 mkdir -p ~/.ssh
-ln -sf ~/.shell/ssh_config ~/.ssh/config
+ln -sf ~/.shell/confs/ssh_config ~/.ssh/config
+
+echo "Setting up Slate"
+ln -sf ~/.shell/confs/slate ~/.slate
+
