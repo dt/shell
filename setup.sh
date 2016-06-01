2b28a9c1c1c5df5984e5277c1e3719f614c65b26
David Taylor <tinystatemachine@gmail.com>
2016-06-01 17:49:16 +0000

    no log messages

--- setup.sh
+++ setup.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-
-echo "Setting up bash scripts"
 ln -sf ~/.shell/profile.sh ~/.profile
 ln -sf ~/.shell/profile.sh ~/.bash_profile
 ln -sf ~/.shell/bashrc.sh ~/.bashrc
 ln -sf ~/.shell/inputrc ~/.inputrc
 
-echo "Setting up git configs scripts"
 ln -sf ~/.shell/confs/gitconfig ~/.gitconfig
 ln -sf ~/.shell/confs/gitignore_global ~/.gitignore_global
 
-echo "Setting up ssh config"
 mkdir -p ~/.ssh
 ln -sf ~/.shell/confs/ssh_config ~/.ssh/config
 
-echo "Setting up Slate"
 ln -sf ~/.shell/confs/slate ~/.slate
 
