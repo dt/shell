7693b0f624e14a2cd45ab6cbc5f506d51afa44d3
David Taylor <tinystatemachine@gmail.com>
2016-06-07 15:47:07 +0000

    move gcloud to homebrew location

--- lang.d/google.sh
+++ lang.d/google.sh
@@ -1,11 +1,11 @@
 
-if [[ -d /Users/david/code/google-cloud-sdk ]]; then
+if [[ -d /opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk ]]; then
 
 	# The next line updates PATH for the Google Cloud SDK.
-	source '/Users/david/code/google-cloud-sdk/path.bash.inc'
+	source '/opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc'
 
 	# The next line enables shell command completion for gcloud.
-	source '/Users/david/code/google-cloud-sdk/completion.bash.inc'
+	source '/opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc'
 fi
 
 if [[ -f /.active ]]; then
