9c623aa683937800a8bf4f31671a7b23f2789c86
David Taylor <tinystatemachine@gmail.com>
2017-02-21 19:13:16 +0000

    move to std gopath

--- lang.d/go.sh
+++ lang.d/go.sh
@@ -1,11 +1,11 @@
-export GOPATH="$HOME/code/go"
-if [ -f "$HOME/.gce" ]; then
-	export GOPATH="$HOME/code/gce"
-fi
+export GOPATH="$HOME/go"
 export PATH=$PATH:$GOPATH/bin
 
 if [[ -d ~/goroot/bin ]]; then
     export PATH=~/goroot/bin:$PATH
+elif [[ -d /usr/local/opt/go/libexec ]]; then
+	export GOROOT="/usr/local/opt/go/libexec"
+    export PATH="/usr/local/opt/go/libexec/bin:$PATH"
 elif [[ -d /usr/local/go/bin ]]; then
 	export GOROOT="/usr/local/go"
     export PATH="/usr/local/go/bin:$PATH"
