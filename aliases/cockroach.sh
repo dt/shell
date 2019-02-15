830a51933efaafc4441e7a5dc85872d3a7ff3d96
David Taylor <tinystatemachine@gmail.com>
2019-02-15 18:45:23 +0000

    add lic env, updated vscode settings and cpp vars

--- aliases/cockroach.sh
+++ aliases/cockroach.sh
@@ -5,26 +5,9 @@ if [[ -d "${GOPATH}/src/github.com/cockroachlabs/production/crl-prod" ]]; then
 fi
 
 if [ -f ~/.roachprod/bash-completion.sh ]; then
-  # The completion func is re-generated on sync to include current clusters.
-  # This is a wrapper func that:
-  #   a) re-reads he original file to re-define the real func
-  #   b) overrides the registration back to our wrapper (the file registers it as well as defining it).
-  #   c) call the original func
-  __reload_start_roachprod()
-  {
-    . ~/.roachprod/bash-completion.sh
-    if [[ $(type -t compopt) = "builtin" ]]; then
-        complete -o default -F __reload_start_roachprod roachprod
-    else
-        complete -o default -o nospace -F __reload_start_roachprod roachprod
-    fi
-    __start_roachprod
-  }
+  . ~/.roachprod/bash-completion.sh
+fi
 
-  if [[ $(type -t compopt) = "builtin" ]]; then
-      complete -o default -F __reload_start_roachprod roachprod
-  else
-      complete -o default -o nospace -F __reload_start_roachprod roachprod
-  fi
-
+if [ -f ~/.cockroach.lic ]; then
+  export COCKROACH_DEV_LICENSE="$(cat ~/.cockroach.lic)"
 fi
--- lang.d/cpp.sh
+++ lang.d/cpp.sh
@@ -1,3 +1,6 @@
 if [[ -d /usr/local/opt/ccache/libexec ]]; then
     export PATH=/usr/local/opt/ccache/libexec:$PATH
 fi
+
+  export LDFLAGS="-L/usr/local/opt/libedit/lib"
+  export CPPFLAGS="-I/usr/local/opt/libedit/include"
