b236d97d8ec971d42e4700ced14397b4325549f5
David Taylor <tinystatemachine@gmail.com>
2019-03-01 18:13:12 +0000

    Add cockroach bin dir to PATH and workload completions

--- aliases/cockroach.sh
+++ aliases/cockroach.sh
@@ -3,9 +3,16 @@ alias csql='psql postgresql://root@localhost:26257?sslmode=disable'
 if [[ -d "${GOPATH}/src/github.com/cockroachlabs/production/crl-prod" ]]; then
   export PATH=${PATH}:${GOPATH}/src/github.com/cockroachlabs/production/crl-prod
 fi
+if [[ -d "${GOPATH}/src/github.com/cockroachdb/cockroach/bin" ]]; then
+  export PATH=${PATH}:${GOPATH}/src/github.com/cockroachdb/cockroach/bin
+fi
 
 if [ -f ~/.roachprod/bash-completion.sh ]; then
   . ~/.roachprod/bash-completion.sh
+fi
+
+if [ -f ~/.workload.bash ]; then
+  . ~/.workload.bash
 fi
 
 if [ -f ~/.cockroach.lic ]; then
