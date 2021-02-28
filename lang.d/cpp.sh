0a323bb7457016adaf03f6e2ed86f2d06fb53b63
David Taylor <tinystatemachine@gmail.com>
2021-02-28 16:04:07 +0000

    only set libedit ldflag if installed

--- lang.d/cpp.sh
+++ lang.d/cpp.sh
@@ -6,5 +6,8 @@ if [[ -d /usr/lib/ccache ]]; then
     export PATH="/usr/lib/ccache:${PATH}"
 fi
 
-export LDFLAGS="-L/usr/local/opt/libedit/lib"
-export CPPFLAGS="-I/usr/local/opt/libedit/include"
+if [[ -d /usr/local/opt/libedit ]]; then
+    export LDFLAGS="-L/usr/local/opt/libedit/lib"
+    export CPPFLAGS="-I/usr/local/opt/libedit/include"
+fi
+
