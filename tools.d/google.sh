a24efb5c7e5460fbe0e5dbf74f7462afdf08d15e
David Taylor <tinystatemachine@gmail.com>
2021-08-22 20:25:06 +0000

    Add non-m1 brew path to google sdk

--- tools.d/google.sh
+++ tools.d/google.sh
@@ -1,4 +1,8 @@
+if [[ -d /usr/local/Caskroom/google-cloud-sdk/ ]]; then
+  source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+  source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
 
+fi
 if [[ -d /opt/homebrew/Caskroom/google-cloud-sdk ]]; then
   source "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
   source "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
